3f05bc43c5759913591766664da1440f40218839
Michael <zl2mjp@gmail.com>
2016-05-28 17:36:01 +1200

    add to string methods for priority enum

--- src/kps/xml/objects/enums/Priority.java
+++ src/kps/xml/objects/enums/Priority.java
@@ -8,4 +8,18 @@ public enum Priority {
     @XmlEnumValue("Domestic Standard") DOMESTIC_STANDARD,
     @XmlEnumValue("International Air") INTERNATIONAL_AIR,
     @XmlEnumValue("International Standard") INTERNATIONAL_STANDARD;
+
+    @Override
+    public String toString() {
+        switch(this) {
+            case DOMESTIC_STANDARD:
+            default:
+                return "Domestic standard";
+            case INTERNATIONAL_AIR:
+                return "International air";
+            case INTERNATIONAL_STANDARD:
+                return "International standard";
+
+        }
+    }
 }
